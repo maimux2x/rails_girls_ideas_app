4671938c7ccb9b799834e326d7088da3ae1c6bbc
maimux2x <m0smile10often@gmail.com>
2024-05-06 18:33:53 +0900

    update app

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root to: redirect("/ideas")
+  root "pages#homepage"
+  get 'pages/about'
   resources :ideas
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
